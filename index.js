8e786fe69be0260c60afb095a98fcc74da29321d
FutureScript <fernandobaxter1@gmail.com>
2018-07-01 01:31:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
-
+`
 var conversation = "Let's talk about " + '${topic}';
 
 
 
 
+`
